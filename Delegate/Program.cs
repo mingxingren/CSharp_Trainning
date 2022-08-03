16b3c4ead55797512d1d50d46fdfb96d2eee7bc3
mamingkun <mamingkun3@gmail.com>
2022-08-03 11:45:15 +0800

    1. commit code

---
+++ Delegate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Program {
+    class App {
+        static void Main(string[] args) {
+            Func<int, bool> fnPrint = (int param) => {
+                Console.WriteLine("this is Func");
+                return param % 2 == 0;
+            };
+
+            fnPrint(0);
+
+            Action<int> fnAction = (int param) => {
+                Console.WriteLine("this is Action");
+            };
+            fnAction(0);
+
+            Predicate<int> fnPredicate = (int param) => {
+                Console.WriteLine("this is Predicate");
+                return param % 2 == 0;
+            };
+            fnPredicate(0); 
+        }
+    }
+}
---
+++ MultiThread/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MultiThread
+{
+    class Program {
+        static void Main(string[] args) {
+            MultiThread thd = new MultiThread();
+            Task.Run(thd.Run);
+            Thread.Sleep(100);
+            thd.Wait();
+        }
+    }
+
+    class MultiThread {
+        private readonly Object m_mutex = new Object();
+
+        public MultiThread() {
+
+        }
+
+        public void Run() {
+            lock(m_mutex) {
+                Console.WriteLine("Thread sleep 3 seconds");
+                Thread.Sleep(1000 * 3);
+            }
+            Console.WriteLine("release mutex");
+        }
+
+        public void Wait() {
+            Console.WriteLine("Try to get mutex");
+            lock(m_mutex) {
+                Console.WriteLine("Get mutex!");
+            }
+        }
+    }    
+}
